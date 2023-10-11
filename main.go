031b5e034d9e3bf5748e50c04cb2487c2e547f42
vaaski <admin@vaa.ski>
2023-10-11 18:06:11 +0200

    ask for format first

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/url"
 	"os"
 	"os/exec"
 	"runtime"
@@ -11,7 +10,6 @@ import (
 	"github.com/buger/jsonparser"
 	"github.com/erikgeiser/promptkit/selection"
 	"github.com/erikgeiser/promptkit/textinput"
-	"golang.design/x/clipboard"
 )
 
 var DEFAULT_ARGS = [...]string{"--force-keyframes-at-cuts", "-P", "ytdl-download", "--embed-metadata", "--no-playlist", "--console-title"}
@@ -21,34 +19,30 @@ var PRESET_MAP = map[string][]string{
 	"mp3":      {"-x", "--audio-format", "mp3", "-o", "%(uploader)s - %(title)s.%(ext)s"},
 }
 
-func validateUrl(inputUrl string) bool {
-	parsed, err := url.ParseRequestURI(inputUrl)
-	return err == nil && parsed.Scheme != "" && parsed.Host != ""
-}
-
-func getClipboardUrl() string {
-	err := clipboard.Init()
-	maybePanic(err)
-
-	text := clipboard.Read(clipboard.FmtText)
-	stringified := string(text)
-	validUrl := validateUrl(stringified)
-
-	if validUrl {
-		return stringified
-	} else {
-		return ""
-	}
-}
-
 func main() {
 	dynamicArgs := []string{}
 
-	inputPrompt := textinput.New("url:")
+	println("enter a query, either a", formatBold("url"), "or something to", formatBold("search on youtube"))
+
+	inputPrompt := textinput.New("query:")
 	inputPrompt.InitialValue = getClipboardUrl()
 
 	url, inputErr := inputPrompt.RunPrompt()
 	if inputErr != nil {
+		os.Exit(0)
+	}
+
+	presets := make([]string, len(PRESET_MAP))
+	i := 0
+	for k := range PRESET_MAP {
+		presets[i] = k
+		i++
+	}
+
+	presetPicker := selection.New("preset:", presets)
+	presetPicker.Filter = nil
+	preset, presetErr := presetPicker.RunPrompt()
+	if presetErr != nil {
 		os.Exit(0)
 	}
 
@@ -61,51 +55,30 @@ func main() {
 	cpuCount := runtime.NumCPU()
 	dynamicArgs = append(dynamicArgs, "-N", fmt.Sprint(cpuCount))
 
-	// println("Input url:", inputUrl)
-
 	infoArgs := append(DEFAULT_ARGS[:], "-J")
 	infoArgs = append(infoArgs, dynamicArgs...)
 	infoArgs = append(infoArgs, inputUrl)
 
-	// println("finalArgs:", strings.Join(infoArgs, " "))
 	infoCmd := exec.Command("yt-dlp", infoArgs...)
 	infoOut, infoErr := infoCmd.Output()
 	maybePanic(infoErr)
 
-	// os.WriteFile("info.json", infoOut, 0644)
+	//? if it's a playlist (like when you search for a song), just use the first entry
+	firstEntry, _, _, entryErr := jsonparser.Get(infoOut, "entries", "[0]")
+	if firstEntry != nil && entryErr == nil {
+		infoOut = firstEntry
+	}
 
-	title, titleErr := jsonparser.GetString(infoOut, "entries", "[0]", "title")
+	title, titleErr := jsonparser.GetString(infoOut, "title")
 	if titleErr != nil {
-
-		title, titleErr = jsonparser.GetString(infoOut, "title")
-		if titleErr != nil {
-			title = "error extracting title"
-		}
+		title = "error extracting title"
 	}
 
-	println("title:", title)
-
-	presets := make([]string, len(PRESET_MAP))
-	i := 0
-	for k := range PRESET_MAP {
-		presets[i] = k
-		i++
-	}
-
-	presetPicker := selection.New("preset", presets)
-	presetPicker.Filter = nil
-	preset, presetErr := presetPicker.RunPrompt()
-	if presetErr != nil {
-		os.Exit(0)
-	}
-
-	// println("selected preset:", strings.Join(PRESET_MAP[preset], " "))
+	println("title:", formatColor(title))
 
 	downloadArgs := append(DEFAULT_ARGS[:], PRESET_MAP[preset]...)
 	downloadArgs = append(downloadArgs, dynamicArgs...)
 	downloadArgs = append(downloadArgs, "--load-info-json", "-")
-
-	println("finalArgs:", strings.Join(downloadArgs, " "))
 
 	downloadCmd := exec.Command("yt-dlp", downloadArgs...)
 	downloadCmd.Stdin = strings.NewReader(string(infoOut))
--- util.go
+++ util.go
@@ -1,7 +1,42 @@
 package main
 
+import (
+	"net/url"
+
+	"github.com/muesli/termenv"
+	"golang.design/x/clipboard"
+)
+
 func maybePanic(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func validateUrl(inputUrl string) bool {
+	parsed, err := url.ParseRequestURI(inputUrl)
+	return err == nil && parsed.Scheme != "" && parsed.Host != ""
+}
+
+func formatColor(input string) string {
+	return termenv.String(input).Foreground(termenv.ANSI256Color(32)).String()
+}
+
+func formatBold(input string) string {
+	return termenv.String(input).Bold().String()
+}
+
+func getClipboardUrl() string {
+	err := clipboard.Init()
+	maybePanic(err)
+
+	text := clipboard.Read(clipboard.FmtText)
+	stringified := string(text)
+	validUrl := validateUrl(stringified)
+
+	if validUrl {
+		return stringified
+	} else {
+		return ""
 	}
 }
